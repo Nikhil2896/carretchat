7fe8dd93895a2f91809e54b79ea0e0c95cf9a11b
Sai Nikhil Patnam <patnamsainikhil@gmail.com>
2025-10-28 02:35:50 +0530

    textinput added, messages sent to messages array

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,24 +1,113 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import Theme from '../constants/Theme';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const InputView = props => {
+  const [newMessage, setNewMessage] = useState();
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    let msg = newMessage?.trim();
+    if (msg && msg.length > 0) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [newMessage]);
+
+  const onSend = msg => {
+    const date = new Date();
+    const messageId = date.getTime();
+    let messageData = {
+      sender: props.name,
+      messageId: messageId,
+      timeStamp: date,
+      senderId: 12345,
+      message: newMessage,
+    };
+    props.setMessageList(prev => [...prev, messageData]);
+    setNewMessage();
+  };
+  return (
+    <View style={styles.sendView}>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setNewMessage}
+        value={newMessage}
+        placeholder="Your message....."
+        placeholderTextColor={Theme.colors.placeHolder}
+        maxLength={20}
+        selectionColor={Theme.colors.primaryColor}
+      />
+      <View style={styles.sendButton}>
+        <Icon
+          name={'paper-plane'}
+          size={20}
+          style={{ color: Theme.colors.primaryLight }}
+          onPress={() => onSend(newMessage)}
+          disabled={disabled}
+        />
+      </View>
+    </View>
+  );
+};
 
 const Chat = props => {
+  const [messageList, setMessageList] = useState([]);
+
   useEffect(() => {
-    console.log(props.route.params);
+    const date = new Date();
+    const messageId = date.getTime();
+    let firstMessage = {
+      sender: props.route.params.userData.name,
+      messageId: messageId,
+      timeStamp: date,
+      senderId: props.route.params.userData.id,
+      message: 'Hey!!!',
+    };
+    setMessageList([...messageList, firstMessage]);
   }, []);
+
+  const MessagesView = () => {
+    return <View style={{ flex: 1 }}></View>;
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Chat</Text>
+      <MessagesView />
+      <InputView
+        name={props.route.params.name}
+        setMessageList={setMessageList}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
     flex: 1,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: Theme.colors.primaryLight,
+    paddingHorizontal: 20,
+  },
+  sendView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  textInput: {
+    borderWidth: 2,
+    borderRadius: 100,
+    borderColor: Theme.colors.primaryColor,
+    paddingHorizontal: 15,
+    color: Theme.colors.primaryDark,
+    flex: 1,
+    marginRight: 10,
+  },
+  sendButton: {
+    backgroundColor: Theme.colors.primaryColor,
+    padding: 10,
+    borderRadius: 50,
   },
 });
 
